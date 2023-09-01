dd42b8dc32180947cd97f52b465e332e7073848d
ZubaerJewel <zubaer5jewel.16@gmail.com>
2023-09-01 12:21:56 +0600

    server side for socket io

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+const PORT = process.env.PORT || 3001
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+const conversationMap = new Map(); 
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+
+    const roomConversation = conversationMap.get(data) || [];
+    socket.emit("previous_conversation", roomConversation);
+  });
+
+  socket.on("get_conversation", (room) => {
+    const roomConversation = conversationMap.get(room) || [];
+    socket.emit("previous_conversation", roomConversation);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+
+    if (!conversationMap.has(data.room)) {
+      conversationMap.set(data.room, []);
+    }
+    conversationMap.get(data.room).push(data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("SERVER RUNNING for chat app by socket io");
+});
+
